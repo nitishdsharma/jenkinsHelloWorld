bc81c1f72a392835adc77d308343f5a01124d4b3
Nitish Sharma <46995651+nitishdsharma@users.noreply.github.com>
2020-05-31 16:33:08 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String args[]) {
         System.out.println("Java Hello World");
+        System.out.println("Training has started, we made some changes");
     }
 }
